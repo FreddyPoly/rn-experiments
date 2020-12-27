4f2bcce711327aa2de8bfcb87e39c6753e093150
FreddyPoly <wolfensteinspirit@hotmail.fr>
2020-12-27 20:20:29 +0100

    test avec ScrollView

--- app/screens/demo-carousel-1/index.js
+++ app/screens/demo-carousel-1/index.js
@@ -1,10 +1,49 @@
 import React, { useState, useRef } from 'react';
-import { View, Text, StyleSheet, Animated, TouchableOpacity, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Animated, TouchableOpacity, Dimensions, ScrollView } from 'react-native';
+
+const HEIGHT = 200;
+const WIDTH = 150;
+const elements = [
+  {
+    color: 'blue',
+    title: 'Titre 1',
+  },
+  {
+    color: 'red',
+    title: 'Titre 1',
+  },
+  {
+    color: 'yellow',
+    title: 'Titre 1',
+  },
+];
 
 const DemoCarousel1 = () => {
+  const [activePage, setActivePage] = useState(0);
+
+  const onScrollEndDrag = (data) => {
+    console.log(`data : ${JSON.stringify(data.nativeEvent)}`);
+    console.log(`-- page : ${data.nativeEvent.contentOffset.x} / ${WIDTH / 2} = ${data.nativeEvent.contentOffset.x % (WIDTH / 2)}`);
+
+    setActivePage(data.nativeEvent.contentOffset.x / WIDTH / 2);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>CAROUSEL 1</Text>
+      <View style={{ height: HEIGHT, width: WIDTH }}>
+        <ScrollView
+          style={{ flex: 1 }}
+          pagingEnabled
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          onScrollEndDrag={onScrollEndDrag}>
+          {elements.map((element, index) => (
+            <View key={index} style={{ backgroundColor: element.color, height: HEIGHT, width: WIDTH }} />
+          ))}
+        </ScrollView>
+      </View>
+
+      <Text>Page {activePage}</Text>
     </View>
   );
 };
@@ -12,7 +51,7 @@ const DemoCarousel1 = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
 });
